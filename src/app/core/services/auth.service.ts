291108b33b9c7b850fb931e1ed296636ef42b4df
Anameskhi <ameskhi@unisens.ge>
2022-12-14 11:44:50 +0400

    added authentication login and sign up

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { AuthRoutingModule } from 'src/app/pages/auth/auth-routing.module';
 import { environment } from 'src/environments/environment';
 import { Auth, AuthResponse } from '../interfaces/auth';
 
@@ -20,9 +21,34 @@ export class AuthService {
 
   signUp(params: Auth): Observable<AuthResponse>{
     return this.http.post<AuthResponse>(`${this.firebaseAuthUrl}signUp?key=${this.apiKey}`,params)
+    .pipe(
+      tap((authResponse: AuthResponse)=>{
+        this.setAuth(authResponse)
+      })
+    )
   }
 
   signIn(params: Auth): Observable<AuthResponse>{
     return this.http.post<AuthResponse>(`${this.firebaseAuthUrl}signInWithPassword?key=${this.apiKey}`,params)
+  .pipe(
+    tap((authResponse: AuthResponse)=>{
+      this.setAuth(authResponse)
+    })
+  )
   }
+
+  setAuth(authResponse: AuthResponse):void{
+    localStorage.setItem('token',authResponse.idToken)
+    localStorage.setItem('refreshtoken',authResponse.refreshToken)
+    localStorage.setItem('email',authResponse.email)
+
+  }
+  get isAuth():boolean{
+    return !!localStorage.getItem('token')
+  }
+
+  logout(): void{
+    localStorage.clear()
+  }
+  
 }
